e7cd3f93a1a6303b1e0d6c74a550f41cd7e2adf6
nguy0907 <nguy0907@algonquinlive.com>
2021-12-07 03:51:30 -0500

    adding js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-fetch('https://api.nasa.gov/planetary/apod?api_key=f41M4YrhaDGpFaIMrptDsj5a0DXCrB6tN29ajYmp&date=2021-12-4&thumbs=True')
+//data
+const $today = new Date()
+const $dateDisplay = document.getElementById('dateDisplay')
+const $dateTimer = document.getElementById('dateTimer')
+
+$dateDisplay.innerHTML = $today.toDateString()
+
+
+//Timer Section
+
+
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=f41M4YrhaDGpFaIMrptDsj5a0DXCrB6tN29ajYmp&date=2021-12-7&thumbs=True')
+
+
+
 
 .then(function (response){
     return response.json()
@@ -15,3 +30,6 @@ if (imageData.media_type === 'video'){
     document.querySelector('img').setAttribute('src', imageData.hdurl)
 }
 })
+
+
+//Timer Section
