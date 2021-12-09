8a8b9e7629b5799453623eb803c4ad244b0c916e
nguy0907 <nguy0907@algonquinlive.com>
2021-12-09 15:31:42 -0500

    adding js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //data
 const today = new Date()
-const future = new Date()
 const $dateDisplay = document.getElementById('dateDisplay')
 const $dateTimer = document.getElementById('dateTimer')
 const $dateTimeDisplay = document.getElementById('dateTimeDisplay')
 const $timerDate = document.getElementById('timerDate')
+const $dateOptions = document.getElementById('dateOptions')
+const $settings = document.getElementById('settings')
 
 
-$dateDisplay.innerHTML = today.toDateString()
 
 
 //Timer Section
@@ -41,14 +41,69 @@ function toSeconds(ms){
 timerCounting = setInterval(function (){
     timerCount +=1000
 
-    $timerDate.innerHTML =`${toHours(timerCount)}  ${toMinutes(timerCount)} ${toSeconds(timerCount)}`
+    $timerDate.innerHTML =`
+    <div> 
+    <p> Minutes ${toMinutes(timerCount)}</p> 
+    <p> Seconds ${toSeconds(timerCount)} </p>
+    </div>
+    
+  `
 },1000
 )
 
+$settings.addEventListener('submit', function(event){
+    event.preventDefault()
 
-$dateTimeDisplay.innerHTML = today
+    // This is the option for the date display
+    for (let i=0; i<$settings.elements.length; i++){
+        console.log($settings.elements['date'].value)
+    }
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=f41M4YrhaDGpFaIMrptDsj5a0DXCrB6tN29ajYmp&date=2021-12-7&thumbs=True')
+switch ($settings.elements['date'].value){
+    case '0':
+        $dateDisplay.innerHTML = today.toDateString()
+        $dateTimeDisplay.innerHTML = ''
+    break
+
+    case '1':
+// $dateTimeDisplay.innerHTML = today
+$dateDisplay.innerHTML = ''
+    break
+
+  
+}
+  //This is the option for the time display
+
+  for (let i=0; i<$settings.elements.length; i++){
+    console.log($settings.elements['time'].value)
+}
+
+switch ($settings.elements['time'].value){
+case '0':
+    $timerDate.innerHTML =`
+    <div> 
+    <p> Minutes ${toMinutes(timerCount)}</p> 
+    <p> Seconds ${toSeconds(timerCount)} </p>
+    </div> 
+  `
+break
+
+case '1':
+    $timerDate.innerHTML =`
+    <div> 
+    <p> Hours ${toHours(timerCount)} </p> 
+    <p> Minutes   ${toMinutes(timerCount)}</p> 
+    <p> Seconds ${toSeconds(timerCount)} </p>
+    </div>
+  `
+break
+
+}
+})
+
+
+
+fetch('https://api.nasa.gov/planetary/apod?api_key=f41M4YrhaDGpFaIMrptDsj5a0DXCrB6tN29ajYmp&date=2021-12-9&thumbs=True')
 
 
 
