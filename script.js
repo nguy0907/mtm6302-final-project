408251e30448d53f55ed77d2a9433160c75aeb6e
nguy0907 <nguy0907@algonquinlive.com>
2021-12-09 12:02:32 -0500

    adding js

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 //data
-const $today = new Date()
+const today = new Date()
+const future = new Date()
 const $dateDisplay = document.getElementById('dateDisplay')
 const $dateTimer = document.getElementById('dateTimer')
+const $dateTimeDisplay = document.getElementById('dateTimeDisplay')
+const $timerDate = document.getElementById('timerDate')
 
-$dateDisplay.innerHTML = $today.toDateString()
+
+$dateDisplay.innerHTML = today.toDateString()
 
 
 //Timer Section
+let timerCount = today.getTime()
+
+function toDays(ms) {
+    return Math.floor(ms / 1000 / 60 / 60 / 24)
+}
+function toHours(ms){
+    const days = toDays(ms)
+    const hours = Math.floor(ms / 1000 / 60 / 60)
+    const hoursadd = hours - (days * 24)
+
+   return hoursadd
+}
+
+function toMinutes(ms){
+    const days = toDays(ms)
+    const hours = toHours(ms)
+    const minutes = Math.floor(ms / 1000 / 60)
+    const minutesadd = minutes - (hours * 60) - (days * 24 * 60)
+
+    return minutesadd
+}
+
+function toSeconds(ms){
+    const seconds = Math.floor((ms % (1000 * 60)) / 1000)
+    return seconds
+}
+
+timerCounting = setInterval(function (){
+    timerCount +=1000
+
+    $timerDate.innerHTML =`${toHours(timerCount)}  ${toMinutes(timerCount)} ${toSeconds(timerCount)}`
+},1000
+)
 
 
+$dateTimeDisplay.innerHTML = today
 
 fetch('https://api.nasa.gov/planetary/apod?api_key=f41M4YrhaDGpFaIMrptDsj5a0DXCrB6tN29ajYmp&date=2021-12-7&thumbs=True')
 
